97270af5ba607b6a794cd6e31d5b7688ab079db6
NeerajGupta23 <230803neeraj@gmail.com>
2024-09-19 18:48:12 +0530

    UI of homepage and User page is enhanced

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './src/**/*.vue',
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+    "./src/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "backgroud-color1": "#090030",
+      },
+    },
   },
   plugins: [],
-}
+};
